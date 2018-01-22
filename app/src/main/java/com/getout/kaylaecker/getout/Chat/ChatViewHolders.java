8fe54a6a158731e84b24ec53d8269873296e75ba
Kayla Ecker <kaylajayecker@gmail.com>
2018-01-21 19:44:46 -0800

    working/showing messages between matched users

--- app/src/main/java/com/getout/kaylaecker/getout/Chat/ChatViewHolders.java
+++ app/src/main/java/com/getout/kaylaecker/getout/Chat/ChatViewHolders.java
@@ -2,15 +2,24 @@ package com.getout.kaylaecker.getout.Chat;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.getout.kaylaecker.getout.R;
 
 /**
  * Created by kaylaecker on 1/21/18.
  */
 
 public class ChatViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public TextView mMessage;
+    public LinearLayout mContainer;
     public ChatViewHolders(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
+
+        mMessage = itemView.findViewById(R.id.message);
+        mContainer = itemView.findViewById(R.id.container);
     }
 
     @Override
